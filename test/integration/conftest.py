de23051241252af8bf2d6b64cc5e5ffa3681414d
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2019-12-17 01:55:32 +0530

    Adding integration tests

---
+++ test/integration/conftest.py
@@ -0,0 +1,43 @@
+import logging
+import os
+import sqlite3
+
+import pytest
+
+from test.common.utilities import get_mock_qbo, qbo_connect
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def mock_qbo():
+    """
+    Returns mock QBO instance
+    """
+    return get_mock_qbo()
+
+
+@pytest.fixture
+def dbconn():
+    sqlite_db_file = '/tmp/test_qbo.db'
+    if os.path.exists(sqlite_db_file):
+        os.remove(sqlite_db_file)
+    return sqlite3.connect(sqlite_db_file, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+
+
+@pytest.fixture(scope='module')
+def qbo_extract():
+    """
+    Returns QBO extract instance
+    """
+    return qbo_connect(dbconn)
+
+
+@pytest.fixture
+def qbo(qbo_extract, dbconn):
+    """
+    Return QBO Extract connector objects
+    """
+    res = qbo_extract
+    res.create_tables()
+    return res
